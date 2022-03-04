acc3d61f02b5ab7d0838b9099ee569f72a3651bf
pvucic <100967826+pvucic@users.noreply.github.com>
2022-03-04 23:25:37 +0100

    Update TC-RP-013.js

--- cypress/integration/assignment/TC-RP-013.js
+++ cypress/integration/assignment/TC-RP-013.js
@@ -15,5 +15,32 @@ describe("LINKS Project", function () {
     cy.get('[name="DateOfBirthYear"]')
       .select("1992")
       .should("have.value", "1992");
+
+    for (var i = 0; i < 32; i++) {
+      cy.get('[name="DateOfBirthDay"]').select(i).should("have.value", i);
+    }
+
+    const months = [
+      "siječanj",
+      "veljača",
+      "ožujak",
+      "travanj",
+      "svibanj",
+      "lipanj",
+      "srpanj",
+      "kolovoz",
+      "rujan",
+      "listopad",
+      "studeni",
+      "prosinac",
+    ];
+
+    months.forEach((month) => {
+      cy.get('[name="DateOfBirthMonth"]')
+        .select(month)
+        .find("option:selected")
+        .should("have.text", month);
+    });
   });
 });
+
